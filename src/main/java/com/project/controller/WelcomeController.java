f71c322533732141776af7b5c5bd7c64060b543c
Mahmoud-Magdy-sayed <60424531+Mahmoud-Magdy-sayed@users.noreply.github.com>
2020-02-11 14:47:47 +0200

    till adding basic configuration

--- src/main/java/com/project/controller/WelcomeController.java
+++ src/main/java/com/project/controller/WelcomeController.java
@@ -1,9 +1,13 @@
 package com.project.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.configuration.BasicConfiguration;
 import com.project.service.WelcomeService;
 
 @RestController
@@ -12,8 +16,21 @@ public class WelcomeController {
 	@Autowired
 	private WelcomeService service;
 	
+	@Autowired
+    private BasicConfiguration configuration;
+
 	@RequestMapping("/welcome")
 	public String welcome() {
 		return service.retrieveWelcomeMessage();
 	}
+
+    @RequestMapping("/dynamic-configuration")
+    public Map dynamicConfiguration() {
+        // Not the best practice to use a map to store differnt types!
+        Map map = new HashMap();
+        map.put("message", configuration.getMessage());
+        map.put("number", configuration.getNumber());
+        map.put("key", configuration.isValue());
+        return map;
+    }
 }

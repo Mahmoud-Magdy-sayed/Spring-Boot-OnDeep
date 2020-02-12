8b3f213cd57e7496e0c99f1b53c2f1dddb56a38d
Mahmoud-Magdy-sayed <60424531+Mahmoud-Magdy-sayed@users.noreply.github.com>
2020-02-12 11:52:04 +0200

    connect with DB

---
+++ src/main/java/com/project/jpa/UserRepository.java
@@ -0,0 +1,14 @@
+package com.project.jpa;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+	List<User> findByRole(@Param("role") String role);
+	
+}
--- src/main/java/com/project/springboot/Application.java
+++ src/main/java/com/project/springboot/Application.java
@@ -2,16 +2,27 @@ package com.project.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.project")
+@EntityScan("com.project")
+@EnableJpaRepositories("com.project")
 public class Application {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
+	}
+	@Profile("prod")
+	@Bean
+	public String dummy() {
+		return "something";
 	}
 
 }
